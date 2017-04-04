59f526c97c2733f28932a8391559d89b9dc59f04
John Watts <johnwatts@gmx.com>
2017-04-04 09:42:05 -0500

    added table to db

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+
+
+
+ CREATE TABLE albums (
+
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   artist VARCHAR(150) NOT NULL DEFAULT 'NONE',
+   name VARCHAR(150) NOT NULL DEFAULT 'NONE',
+   release_date YEAR,
+   sales DECIMAL(12, 2),
+   genre CHAR(255) NOT NULL DEFAULT 'UNKNOWN',
+   PRIMARY KEY (id)
+
+ );
