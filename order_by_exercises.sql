77a9252bf966dfa706e44400c3f9a182d73d0690
John Watts <johnwatts@gmx.com>
2017-04-05 14:12:57 -0500

    Completed order by exercises and added completed group by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -5,12 +5,14 @@ FROM employees
 WHERE gender = 'M'
 AND (first_name IN ('Irena','Vidya')
 OR first_name = 'Maya'
-);
+)
+ORDER BY last_name, first_name;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-OR last_name LIKE '%E';
+OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
@@ -20,7 +22,8 @@ AND last_name LIKE '%E';
 SELECT *
 FROM employees
 WHERE hire_date >= '1990'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT *
 FROM employees
