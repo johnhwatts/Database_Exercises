41f09f71c24e95e67090a542dd31a2aadb38b425
John Watts <johnwatts@gmx.com>
2017-04-07 11:53:13 -0500

    adding the SQL joins exercises with my solutions

---
+++ associative_table_joins_on_employees_db.sql
@@ -0,0 +1,45 @@
+USE employees_db;
+
+-- a query that shows each department along with the name of the current manager for that department
+-- Find the name of all departments currently managed by women.
+SELECT dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees as e
+JOIN dept_manager as dm
+  ON dm.emp_no = e.emp_no
+JOIN departments as d
+  ON d.dept_no = dm.dept_no
+ WHERE dm.to_date > now() AND gender = 'F';
+
+-- Find the current titles of employees currently working in the Customer Service department.
+ SELECT title AS 'Title', dept_name AS 'department name', COUNT (*)
+ FROM titles AS t
+ JOIN dept_emp AS de
+  ON t.emp_no = de.emp_no
+ JOIN departments AS d
+  ON d.dept_no = de.dept_no
+ WHERE dept_name = 'Customer Service' AND t.to_date > now()
+ GROUP BY title;
+
+-- Find the current salary of all current managers.
+ SELECT dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', salary
+ FROM employees as e
+ JOIN dept_manager as dm
+   ON dm.emp_no = e.emp_no
+ JOIN departments as d
+   ON d.dept_no = dm.dept_no
+ JOIN salaries as s
+   ON e.emp_no = s.emp_no
+  WHERE dm.to_date > now() AND s.to_date > now();
+
+-- Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', dept_name AS 'Department Name', CONCAT(manager.first_name, ' ', manager.last_name) AS 'Manager Name'
+FROM employees AS e
+JOIN dept_emp AS de
+  ON e.emp_no = de.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN dept_manager AS dm
+  ON dm.dept_no = d.dept_no
+JOIN employees AS manager
+  ON manager.emp_no = dm.emp_no
+WHERE de.to_date > now() AND dm.to_date > now();
